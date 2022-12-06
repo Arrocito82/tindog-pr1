49bc025baa4ee03041d9f70e9ade634c99a82cab
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2022-12-06 03:58:32 +0000

    se añadio servidor express, node app y dev container

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Tindog app listening on port ${port}`)
+})
